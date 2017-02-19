f3ce22e444b897f3a0ff261c8df56bf65905f10c
Jessica_Nebolsky <jessican310@hotmail.com>
2017-02-19 18:06:07 -0500

    made some more scripts n stuff
    added in selling, buying, monetary system, and growing mechanics

---
+++ Farm_Simulator_5000/Assets/scripts/BuySeeds.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuySeeds : MonoBehaviour {
+
+	int cost = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown(){
+		if (playerResources.money >= 0) {
+			FindObjectOfType<playerResources> ().subtractMoney (cost);
+		}
+	}
+}
---
+++ Farm_Simulator_5000/Assets/scripts/growPlants.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class growPlants : MonoBehaviour {
+
+	public Material[] materials;
+	public Renderer rend;
+	int toGrow = 3;
+	float timeLeft;
+	int timer = 30;
+	int cost = 10;
+
+	// Use this for initialization
+	void Start () {
+		rend.sharedMaterial = materials [materials.Length-1];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeLeft -= Time.deltaTime;
+	}
+
+	void OnMouseDown(){
+		if (timeLeft < 0 && toGrow >= 0) {
+			rend.sharedMaterial = materials [toGrow];
+			toGrow -= 1;
+			timeLeft = timer;
+		} 
+		else if (toGrow == 0) {
+		//sell the plant
+			FindObjectOfType<playerResources> ().addMoney (cost);
+		//then I guess just destroy game object
+		}
+	}
+}

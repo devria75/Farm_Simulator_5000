8d6d536ec7b4df788b08f5b824f421f89d1d1b8c
Jessica_Nebolsky <jessican310@hotmail.com>
2017-03-21 01:38:42 -0400

    added documentation
    commented all the code so when Ian goes through it it's easy to
    understand

--- Farm_Simulator_5000/Assets/scripts/growPlants.cs
+++ Farm_Simulator_5000/Assets/scripts/growPlants.cs
@@ -5,33 +5,45 @@ using UnityEngine.UI;
 
 public class growPlants : MonoBehaviour {
 
+	//timer info
 	public float timeLeft;
 	public int timer = 30;
+
+	//player starts not in collider
 	public bool playerInCol = false;
+
+	//game objects being instantiated or destroyed
 	private GameObject plant;
 	public GameObject Carrot;
 	public GameObject Corn;
 	public GameObject Potato;
 	public GameObject Sprout;
 	public GameObject Tomato;
+
+	//variables to keep track of what's in the plot
 	public bool plotFilled;
+	public PlotStatus plantInPlot;
+
+	//prices for all components when sold
 	public enum PlotStatus {
 		Empty=0, Corn=6, Carrot=8, Potato=8, Tomato=10
 	};
-	public PlotStatus plantInPlot;
 
+	//text placement
 	public int dirtVerticalOffset = 100;
 	public Text dirtText;
 	public int sellVerticalOffset = 100;
 	public Text sellText;
 
-
+	//character resources
 	public playerResources characterResources;
 
 	// Use this for initialization
 	void Start () {
+		//start with no plots filled
 		plotFilled = false;
 
+		//get playerResources script on start
 		GameObject playerObj = GameObject.FindGameObjectWithTag ("Player");
 		characterResources = playerObj.GetComponent<playerResources> ();
 	}
@@ -40,7 +52,10 @@ public class growPlants : MonoBehaviour {
 	void Update () {
 		timeLeft -= Time.deltaTime;
 
+		//if player is in collider then check to see what input they press: 1,2,3,4
 		if (playerInCol == true) {
+
+			//CORN
 			if (Input.GetKeyDown("1") && plotFilled == false && characterResources.cornCounter > 0){
 				plant = Instantiate(Corn, transform.position, Quaternion.identity) as GameObject;
 				timeLeft = timer;
@@ -48,6 +63,8 @@ public class growPlants : MonoBehaviour {
 				plotFilled = true;
 				characterResources.cornCounter--;
 			}
+
+			//CARROT
 			if (Input.GetKeyDown("2") && plotFilled == false && characterResources.carrotCounter > 0){
 				plant = Instantiate(Carrot, transform.position+Vector3.down*.3f, Quaternion.identity) as GameObject;
 				timeLeft = timer;
@@ -55,6 +72,8 @@ public class growPlants : MonoBehaviour {
 				plotFilled = true;
 				characterResources.carrotCounter--;
 			}
+
+			//TOMATO
 			if (Input.GetKeyDown("3") && plotFilled == false && characterResources.tomatoCounter > 0){
 				plant = Instantiate(Sprout, transform.position+Vector3.up*.3f, Quaternion.identity) as GameObject;
 				timeLeft = timer;
@@ -62,6 +81,8 @@ public class growPlants : MonoBehaviour {
 				plotFilled = true;
 				characterResources.tomatoCounter--;
 			}
+
+			//POTATO
 			if (Input.GetKeyDown("4") && plotFilled == false && characterResources.potatoCounter > 0){
 				plant = Instantiate(Potato, transform.position+Vector3.up*.1f, Quaternion.identity) as GameObject;
 				timeLeft = timer;
@@ -71,11 +92,13 @@ public class growPlants : MonoBehaviour {
 			}
 		}
 
+		//If plot is filled and time has run out show sell text
 		if (plotFilled == true && timeLeft <= 0 && playerInCol) {
 			Vector3 newPos = sellText.rectTransform.position;
 			newPos.y = sellVerticalOffset;
 			sellText.rectTransform.position = newPos;
 
+			//if player presses F then add the money that each plant is worth (see array at top for amounts)
 			if (Input.GetKeyDown ("f")) {
 				FindObjectOfType<playerResources> ().addMoney ((int)plantInPlot);
 				plantInPlot = PlotStatus.Empty;
@@ -85,6 +108,10 @@ public class growPlants : MonoBehaviour {
 		}
 	}
 
+
+	//Show text
+
+	//on entering trigger zone show dirt text
 	void OnTriggerEnter(Collider Col){
 		if (plotFilled == false) {
 			Vector3 newPos = dirtText.rectTransform.position;
@@ -94,6 +121,7 @@ public class growPlants : MonoBehaviour {
 		playerInCol = true;
 	}
 
+	//on exiting trigger zone hide dirt text and/or sell text
 	void OnTriggerExit(Collider Col){
 		Vector3 newPos = dirtText.rectTransform.position;
 		newPos.y = dirtVerticalOffset * -1;
--- Farm_Simulator_5000/Assets/scripts/playerResources.cs
+++ Farm_Simulator_5000/Assets/scripts/playerResources.cs
@@ -4,13 +4,19 @@ using UnityEngine.UI;
 
 public class playerResources : MonoBehaviour {
 
+	//start with $15
 	public static int money = 15;
 	public Text textElement;
+
 	public GameObject StoreObj;
+
+	//intergers for how many of each seed player has
 	public int carrotCounter = 0;
 	public int cornCounter = 0;
 	public int potatoCounter = 0;
 	public int tomatoCounter = 0;
+
+	//text elements
 	public Text carrotText;
 	public Text cornText;
 	public Text potatoText;
@@ -19,20 +25,17 @@ public class playerResources : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		//instantiate text upon load with how many items each has
 		textElement.text = money.ToString();
 		carrotText.text = carrotCounter.ToString ();
 		cornText.text = cornCounter.ToString ();
 		potatoText.text = potatoCounter.ToString ();
 		tomatoText.text = tomatoCounter.ToString ();
 	}
-
-	void OnGui() {
-		//GUI.Label(new Rect(0,0,100,100), money.ToString());
-
-	}
 	
 	// Update is called once per frame
 	void Update () {
+		//update how many of each element you have
 		textElement.text = money.ToString();
 		carrotText.text = "Carrot seeds: " + carrotCounter.ToString ();
 		cornText.text = "Corn seeds: " + cornCounter.ToString ();
@@ -40,24 +43,26 @@ public class playerResources : MonoBehaviour {
 		tomatoText.text = "Tomato seeds: " + tomatoCounter.ToString ();
 		storeMoneyText.text = "$" + money.ToString ();
 
+		//if I is pressed open the store
 		if(Input.GetKeyDown(KeyCode.I))
 		{
 			OpenStore();
-
-
 		}
 	}
-		
+
+	//Add money to player's money
 	public void addMoney(int Money){
 		money += Money;
 		textElement.text = money.ToString();
 	}
 
+	//subtract money from player's money
 	public void subtractMoney(int Money){
 		money -= Money;
 		textElement.text = money.ToString();
 	}
 
+	//Open store
 	public void OpenStore()
 	{
 		StoreObj.SetActive (!StoreObj.activeSelf);
@@ -75,6 +80,7 @@ public class playerResources : MonoBehaviour {
 			
 	}
 
+	//what happens when each element is bought
 	public void BuyCarrot()
 	{
 		if (money < 5) {
